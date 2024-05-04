33cd15287b568fce3bc9a3d65a29261bab18ad3d
v.gordeev <v.gordeev@scalablesolutions.io>
2024-05-04 14:45:59 +0400

    added Lombok

--- stream-homework/src/main/java/ru/gordeev/Task.java
+++ stream-homework/src/main/java/ru/gordeev/Task.java
@@ -1,46 +1,17 @@
 package ru.gordeev;
 
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
 public class Task {
+
     private int id;
+
     private String name;
+
     private TaskStatus status;
-
-    public Task(int id, String name, TaskStatus status) {
-        this.id = id;
-        this.name = name;
-        this.status = status;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public TaskStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(TaskStatus status) {
-        this.status = status;
-    }
-
-    @Override
-    public String toString() {
-        return "Task{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", status=" + status +
-                '}';
-    }
 }
