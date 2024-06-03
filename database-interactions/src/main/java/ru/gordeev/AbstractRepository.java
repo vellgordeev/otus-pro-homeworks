bb194c496fd983833d2088cd4b8ee5d903a34d89
v.gordeev <v.gordeev@scalablesolutions.io>
2024-06-04 01:50:51 +0400

    edit with review

--- database-interactions/src/main/java/ru/gordeev/AbstractRepository.java
+++ database-interactions/src/main/java/ru/gordeev/AbstractRepository.java
@@ -1,8 +1,10 @@
 package ru.gordeev;
 
+import lombok.Getter;
 import ru.gordeev.exceptions.RepositoryException;
 
 import java.lang.reflect.Method;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -15,22 +17,35 @@ import static java.util.Objects.requireNonNull;
 public class AbstractRepository<T> {
 
     private final Class<T> cls;
+    private final ClassEntities classEntities;
     private final DataSource dataSource;
     private final String tableName;
-    private List<Method> cachedGetters;
-    private List<Method> cachedSetters;
-    private List<String> fieldNames;
+    private String preparedUpdateAllFields;
+    private String preparedInsert;
 
     public AbstractRepository(DataSource dataSource, Class<T> cls) {
         this.dataSource = dataSource;
         this.cls = cls;
-        tableName = cls.getAnnotation(RepositoryTable.class).title();
+        this.classEntities = new ClassEntities();
+
+        if (!cls.isAnnotationPresent(RepositoryTable.class)) {
+            throw new RepositoryException("Class " + cls.getName() + " must be annotated with @RepositoryTable");
+        }
+
+        try {
+            cls.getDeclaredConstructor();
+        } catch (NoSuchMethodException e) {
+            throw new RepositoryException("Class " + cls.getName() + " must have a default constructor", e);
+        }
+
+        this.tableName = cls.getAnnotation(RepositoryTable.class).title();
         getGettersSettersAndFieldNames(cls);
     }
 
     public List<T> findAll() {
         List<T> resultList = new ArrayList<>();
-        try (PreparedStatement ps = prepareFindAll();
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement ps = prepareFindAll(connection);
              ResultSet rs = ps.executeQuery()) {
             while (rs.next()) {
                 T entity = createEntityFromResultSet(rs);
@@ -43,7 +58,8 @@ public class AbstractRepository<T> {
     }
 
     public T findById(Long id) {
-        try (PreparedStatement ps = prepareFindById()) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement ps = prepareFindById(connection)) {
             ps.setObject(1, requireNonNull(id));
             ResultSet rs = ps.executeQuery();
             if (rs.next()) {
@@ -56,9 +72,10 @@ public class AbstractRepository<T> {
     }
 
     public void create(T entity) {
-        try (PreparedStatement ps = prepareInsert()) {
-            for (int i = 0; i < cachedGetters.size(); i++) {
-                ps.setObject(i + 1, requireNonNull(cachedGetters.get(i).invoke(entity)));
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement ps = prepareInsert(connection)) {
+            for (int i = 0; i < classEntities.getCachedGetters().size(); i++) {
+                ps.setObject(i + 1, requireNonNull(classEntities.getCachedGetters().get(i).invoke(entity)));
             }
             ps.executeUpdate();
         } catch (Exception e) {
@@ -67,7 +84,8 @@ public class AbstractRepository<T> {
     }
 
     public void update(Long id, String field, String value) {
-        try (PreparedStatement ps = prepareUpdate(field)) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement ps = prepareUpdate(field, connection)) {
             ps.setObject(1, requireNonNull(value));
             ps.setObject(2, requireNonNull(id));
             ps.executeUpdate();
@@ -77,15 +95,16 @@ public class AbstractRepository<T> {
     }
 
     public void updateAllFields(Long id, String... values) {
-        if (values.length != fieldNames.size()) {
+        if (values.length != classEntities.getFieldNames().size()) {
             throw new IllegalArgumentException("The number of values must match the number of fields");
         }
 
-        try (PreparedStatement ps = prepareUpdateAllFields()) {
-            for (int i = 0; i < fieldNames.size(); i++) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement ps = prepareUpdateAllFields(connection)) {
+            for (int i = 0; i < classEntities.getFieldNames().size(); i++) {
                 ps.setObject(i + 1, requireNonNull(values[i]));
             }
-            ps.setObject(fieldNames.size() + 1, requireNonNull(id));
+            ps.setObject(classEntities.getFieldNames().size() + 1, requireNonNull(id));
             ps.executeUpdate();
         } catch (Exception e) {
             throw new RepositoryException("Error updating entity", e);
@@ -93,7 +112,8 @@ public class AbstractRepository<T> {
     }
 
     public void deleteById(Long id) {
-        try (PreparedStatement ps = prepareDeleteById()) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement ps = prepareDeleteById(connection)) {
             ps.setObject(1, requireNonNull(id));
             ps.executeUpdate();
         } catch (Exception e) {
@@ -102,66 +122,76 @@ public class AbstractRepository<T> {
     }
 
     public void deleteAll() {
-        try (PreparedStatement ps = prepareDeleteAll()) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement ps = prepareDeleteAll(connection)) {
             ps.executeUpdate();
         } catch (Exception e) {
             throw new RepositoryException("Error deleting all entities", e);
         }
     }
 
-    private PreparedStatement prepareFindAll() throws SQLException {
-       return dataSource.getConnection().prepareStatement(String.format("SELECT * FROM %s", tableName));
+    private PreparedStatement prepareFindAll(Connection connection) throws SQLException {
+        return connection.prepareStatement(String.format("SELECT * FROM %s", tableName));
     }
 
-    private PreparedStatement prepareFindById() throws SQLException {
-        return dataSource.getConnection().prepareStatement(String.format("SELECT * FROM %s WHERE id = ?", tableName));
+    private PreparedStatement prepareFindById(Connection connection) throws SQLException {
+        return connection.prepareStatement(String.format("SELECT * FROM %s WHERE id = ?", tableName));
     }
 
-    private PreparedStatement prepareUpdateAllFields() throws SQLException {
-        StringBuilder query = new StringBuilder("UPDATE ");
-        query.append(tableName).append(" SET ");
+    private PreparedStatement prepareUpdateAllFields(Connection connection) throws SQLException {
+        if (preparedUpdateAllFields == null) {
+            StringBuilder query = new StringBuilder("UPDATE ");
+            query.append(tableName).append(" SET ");
 
-        for (String field : fieldNames) {
-            query.append(field).append(" = ?, ");
+            for (String field : classEntities.getFieldNames()) {
+                query.append(field).append(" = ?, ");
+            }
+
+            query.setLength(query.length() - 2);
+            query.append(" WHERE id = ?;");
+
+            this.preparedUpdateAllFields = query.toString();
         }
 
-        query.setLength(query.length() - 2);
-        query.append(" WHERE id = ?;");
-       return dataSource.getConnection().prepareStatement(query.toString());
+        return connection.prepareStatement(preparedUpdateAllFields);
     }
 
-    private PreparedStatement prepareUpdate(String field) throws SQLException {
-        if (!fieldNames.contains(field)) {
+    private PreparedStatement prepareUpdate(String field, Connection connection) throws SQLException {
+        if (!classEntities.getFieldNames().contains(field)) {
             throw new RepositoryException("There is no such field in the table");
         }
 
         String query = "UPDATE " + tableName + " SET " + field + " = ? WHERE id = ?";
-        return dataSource.getConnection().prepareStatement(query);
+        return connection.prepareStatement(query);
     }
 
-    private PreparedStatement prepareInsert() throws SQLException {
-        StringBuilder query = new StringBuilder("INSERT INTO ");
-        query.append(tableName).append(" (");
+    private PreparedStatement prepareInsert(Connection connection) throws SQLException {
+        if (preparedInsert == null) {
+            StringBuilder query = new StringBuilder("INSERT INTO ");
+            query.append(tableName).append(" (");
 
-        for (String fieldName : fieldNames) {
-            query.append(fieldName).append(", ");
+            for (String fieldName : classEntities.getFieldNames()) {
+                query.append(fieldName).append(", ");
+            }
+            query.setLength(query.length() - 2);
+            query.append(") VALUES (");
+            query.append("?, ".repeat(classEntities.getFieldNames().size()));
+            query.setLength(query.length() - 2);
+            query.append(");");
+
+            this.preparedInsert = query.toString();
         }
-        query.setLength(query.length() - 2);
-        query.append(") VALUES (");
-        query.append("?, ".repeat(fieldNames.size()));
-        query.setLength(query.length() - 2);
-        query.append(");");
 
-        return dataSource.getConnection().prepareStatement(query.toString());
+        return connection.prepareStatement(preparedInsert);
     }
 
-    private PreparedStatement prepareDeleteById() throws SQLException {
-        return dataSource.getConnection().prepareStatement(String.format("DELETE FROM %s WHERE id = ?", tableName));
+    private PreparedStatement prepareDeleteById(Connection connection) throws SQLException {
+        return connection.prepareStatement(String.format("DELETE FROM %s WHERE id = ?", tableName));
 
     }
 
-    private PreparedStatement prepareDeleteAll() throws SQLException {
-        return dataSource.getConnection().prepareStatement(String.format("DELETE FROM %s", tableName));
+    private PreparedStatement prepareDeleteAll(Connection connection) throws SQLException {
+        return connection.prepareStatement(String.format("DELETE FROM %s", tableName));
     }
 
     private void getGettersSettersAndFieldNames(Class<T> cls) {
@@ -174,51 +204,54 @@ public class AbstractRepository<T> {
             throw new RepositoryException("No fields with @RepositoryField annotation found");
         }
 
-        cachedGetters = fields.stream()
-                .map(f -> {
-                    String getterName = "get" + capitalize(f.getName());
-                    try {
-                        return cls.getMethod(getterName);
-                    } catch (NoSuchMethodException e) {
-                        throw new RepositoryException("No getter found for field: " + f.getName(), e);
-                    }
-                }).toList();
+        classEntities.getCachedGetters()
+                .addAll(fields.stream()
+                        .map(f -> {
+                            String getterName = "get" + capitalize(f.getName());
+                            try {
+                                return cls.getMethod(getterName);
+                            } catch (NoSuchMethodException e) {
+                                throw new RepositoryException("No getter found for field: " + f.getName(), e);
+                            }
+                        }).toList());
 
-        cachedSetters = fields.stream()
-                .map(f -> {
-                    String setterName = "set" + capitalize(f.getName());
-                    try {
-                        return cls.getMethod(setterName, f.getType());
-                    } catch (NoSuchMethodException e) {
-                        throw new RepositoryException("No setter found for field: " + f.getName(), e);
-                    }
-                }).toList();
+        classEntities.getCachedSetters()
+                .addAll(fields.stream()
+                        .map(f -> {
+                            String setterName = "set" + capitalize(f.getName());
+                            try {
+                                return cls.getMethod(setterName, f.getType());
+                            } catch (NoSuchMethodException e) {
+                                throw new RepositoryException("No setter found for field: " + f.getName(), e);
+                            }
+                        }).toList());
 
-        if (cachedGetters.isEmpty()) {
+        if (classEntities.getCachedGetters().isEmpty()) {
             throw new RepositoryException("No valid getters found for fields with @RepositoryField annotation");
         }
 
-        if (cachedSetters.isEmpty()) {
+        if (classEntities.getCachedSetters().isEmpty()) {
             throw new RepositoryException("No valid setters found for fields with @RepositoryField annotation");
         }
 
-        fieldNames = fields.stream()
-                .map(f -> {
-                    String fieldName = f.getAnnotation(RepositoryField.class).name();
-                    if (fieldName.isEmpty()) {
-                        throw new RepositoryException("Field name in @RepositoryField annotation cannot be empty for field: " + f.getName());
-                    }
-                    return fieldName;
-                })
-                .toList();
+        classEntities.getFieldNames()
+                .addAll(fields.stream()
+                        .map(f -> {
+                            String fieldName = f.getAnnotation(RepositoryField.class).name();
+                            if (fieldName.isEmpty()) {
+                                return f.getName();
+                            }
+                            return fieldName;
+                        })
+                        .toList());
     }
 
 
     private T createEntityFromResultSet(ResultSet rs) throws Exception {
         T entity = cls.getDeclaredConstructor().newInstance();
-        for (int i = 0; i < fieldNames.size(); i++) {
-            String fieldName = fieldNames.get(i);
-            Method setter = cachedSetters.get(i);
+        for (int i = 0; i < classEntities.getFieldNames().size(); i++) {
+            String fieldName = classEntities.getFieldNames().get(i);
+            Method setter = classEntities.getCachedSetters().get(i);
             setter.invoke(entity, rs.getObject(fieldName));
         }
         Method setId = cls.getMethod("setId", Long.class);
@@ -231,5 +264,18 @@ public class AbstractRepository<T> {
             return str;
         }
         return str.substring(0, 1).toUpperCase() + str.substring(1);
+    }
+
+    @Getter
+    private static class ClassEntities {
+        private final List<Method> cachedGetters;
+        private final List<Method> cachedSetters;
+        private final List<String> fieldNames;
+
+        public ClassEntities() {
+            this.cachedGetters = new ArrayList<>();
+            this.cachedSetters = new ArrayList<>();
+            this.fieldNames = new ArrayList<>();
+        }
     }
 }
