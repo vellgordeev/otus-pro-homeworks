6cd606f9f9bbab37c1dc6b06282616488b82e9e6
Valentin G <deathman777@gmail.com>
2024-06-25 21:47:15 +0500

    fixes with review

--- servlet-calculator/src/main/java/ru.gordeev.web/LoggingFilter.java
+++ servlet-calculator/src/main/java/ru.gordeev.web/LoggingFilter.java
@@ -1,5 +1,8 @@
 package ru.gordeev.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
@@ -7,14 +10,18 @@ import java.io.IOException;
 
 @WebFilter("/*")
 public class LoggingFilter implements Filter {
+    private static final Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
+
+    @Override
     public void init(FilterConfig filterConfig) {}
 
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
-        System.out.println("Request URI: " + req.getRequestURI());
-        System.out.println("Parameters: " + req.getParameterMap().toString());
+        logger.info("Request URI: {}", req.getRequestURI());
+        logger.info("Parameters: {}", req.getParameterMap());
         chain.doFilter(request, response);
     }
 
+    @Override
     public void destroy() {}
 }
