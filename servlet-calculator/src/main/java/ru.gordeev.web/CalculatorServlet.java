92a6fd655f9b94157e21cdfebba56f709e068a79
Valentin G <deathman777@gmail.com>
2024-06-25 21:44:48 +0500

    fixes with review

--- servlet-calculator/src/main/java/ru.gordeev.web/CalculatorServlet.java
+++ servlet-calculator/src/main/java/ru.gordeev.web/CalculatorServlet.java
@@ -7,13 +7,27 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class CalculatorServlet extends HttpServlet {
+
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String path = request.getServletPath();
-        double num1 = Double.parseDouble(request.getParameter("num1"));
-        double num2 = Double.parseDouble(request.getParameter("num2"));
-        double result = 0;
+        double num1, num2, result;
+        try {
+            num1 = Double.parseDouble(request.getParameter("num1"));
+            num2 = Double.parseDouble(request.getParameter("num2"));
+        } catch (NumberFormatException e) {
+            sendErrorMessage(response, HttpServletResponse.SC_BAD_REQUEST, "Invalid number");
+            return;
+        }
 
         switch (path) {
+            case "/div":
+                if (num2 == 0) {
+                    sendErrorMessage(response, HttpServletResponse.SC_BAD_REQUEST, "Division by zero");
+                    return;
+                }
+                result = num1 / num2;
+                break;
             case "/add":
                 result = num1 + num2;
                 break;
@@ -23,24 +37,28 @@ public class CalculatorServlet extends HttpServlet {
             case "/multiply":
                 result = num1 * num2;
                 break;
-            case "/div":
-                if (num2 != 0) {
-                    result = num1 / num2;
-                } else {
-                    response.setContentType("text/html");
-                    PrintWriter out = response.getWriter();
-                    out.println("<html><body>");
-                    out.println("<h1>Division by zero is not allowed</h1>");
-                    out.println("</body></html>");
-                    return;
-                }
-                break;
+            default:
+                sendErrorMessage(response, HttpServletResponse.SC_NOT_FOUND, "Math operation not found");
+                return;
         }
 
+        sendResultMessage(response, result);
+    }
+
+    private void sendResultMessage(HttpServletResponse response, double result) throws IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
         out.println("<h1>Result: " + result + "</h1>");
+        out.println("</body></html>");
+    }
+
+    private void sendErrorMessage(HttpServletResponse response, int status, String message) throws IOException {
+        response.setContentType("text/html");
+        response.setStatus(status);
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
     }
 }
