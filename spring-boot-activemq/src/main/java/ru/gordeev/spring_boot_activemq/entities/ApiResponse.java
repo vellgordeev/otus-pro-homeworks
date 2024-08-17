c0cb02c24e30ab686e2c1a5c64f2cbc046339677
Valentin G <deathman777@gmail.com>
2024-08-17 14:20:09 +0500

    hw done

---
+++ spring-boot-activemq/src/main/java/ru/gordeev/spring_boot_activemq/entities/ApiResponse.java
@@ -0,0 +1,10 @@
+package ru.gordeev.spring_boot_activemq.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ApiResponse {
+    private String status;
+}
